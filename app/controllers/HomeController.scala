d90878a8d045497f0299fce63074c198aec044d0
Brian Froschauer <brianfroschauer@Brians-MacBook-Pro.local>
2019-04-02 20:59:32 -0300

    update home controller and user.proto

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,16 +1,41 @@
 package controllers
 
-
 import com.typesafe.config.Config
-import example.myapp.helloworld.grpc.{GreeterServiceClient, HelloReply, HelloRequest}
+import example.myapp.helloworld.grpc.GreeterServiceClient
 import javax.inject.Inject
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.Forms.mapping
 import play.api.mvc._
 import product.{ProductRequest, ProductServiceClient}
+import repositories.UserRepository
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class HomeController @Inject()(greeterServiceClient: GreeterServiceClient, productServiceClient: ProductServiceClient, config: Config)
+class HomeController @Inject()(greeterServiceClient: GreeterServiceClient,
+                               productServiceClient: ProductServiceClient,
+                               userRepository: UserRepository,
+                               config: Config)
                               (implicit ec: ExecutionContext) extends InjectedController {
+
+  val userForm: Form[CreateUserForm] = Form {
+    mapping(
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText
+    )(CreateUserForm.apply)(CreateUserForm.unapply)
+  }
+
+  def addUser: Action[AnyContent] = Action.async { implicit request =>
+    userForm.bindFromRequest.fold(
+      _ => {
+        Future.successful(Ok)
+      },
+      data => {
+        val user = userRepository.create(data.firstName, data.lastName)
+        user.map(p => Ok(p.id.toString))
+      }
+    )
+  }
 
   def index: Action[AnyContent] = Action.async {
     val request2 = ProductRequest(1)
@@ -18,3 +43,5 @@ class HomeController @Inject()(greeterServiceClient: GreeterServiceClient, produ
     reply2.map(_.name).map(m => Ok(m))
   }
 }
+
+case class CreateUserForm(firstName: String, lastName: String)
