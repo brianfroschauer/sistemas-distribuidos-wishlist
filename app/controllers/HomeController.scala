56e050518eb8876f924fc51589925f1e65f4395f
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2019-04-02 19:01:44 -0300

    Connected to the server

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,18 +2,19 @@ package controllers
 
 
 import com.typesafe.config.Config
-import example.myapp.helloworld.grpc.{ GreeterServiceClient, HelloReply, HelloRequest }
+import example.myapp.helloworld.grpc.{GreeterServiceClient, HelloReply, HelloRequest}
 import javax.inject.Inject
 import play.api.mvc._
+import product.{ProductRequest, ProductServiceClient}
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
-class HomeController @Inject()(greeterServiceClient: GreeterServiceClient, config: Config)
+class HomeController @Inject()(greeterServiceClient: GreeterServiceClient, productServiceClient: ProductServiceClient, config: Config)
                               (implicit ec: ExecutionContext) extends InjectedController {
 
   def index: Action[AnyContent] = Action.async {
-    val request = HelloRequest("Caplin")
-    val reply: Future[HelloReply] = greeterServiceClient.sayHello(request)
-    reply.map(_.message).map(m => Ok(m))
+    val request2 = ProductRequest(1)
+    val reply2 = productServiceClient.getProduct(request2)
+    reply2.map(_.name).map(m => Ok(m))
   }
 }
